d14d88cc78d4d0347b317a2deb226d298591f3df
Gabor Nemeth <etgabo@gmail.com>
2024-12-19 08:49:33 +0000

    Update New-LapsPSSession.ps1

--- PSRemoting To Entra Joined Host with LAPS credentials/New-LapsPSSession.ps1
+++ PSRemoting To Entra Joined Host with LAPS credentials/New-LapsPSSession.ps1
@@ -1,63 +1,70 @@
-<#pulls LAPS password for specified device and opens a powershell session
-requires DeviceLocalCredential.Read.All, Device.Read.All
-#>
 param($ComputerName,$AdminUser)
 
-#you might also need to change your WSMan Trusted Hosts
-if ((get-item WSMan:\localhost\Client\TrustedHosts).value -ne "<placeholder>") {set-item WSMan:\localhost\Client\TrustedHosts "<placeholder>"}
+if ((Get-Item WSMan:\localhost\Client\TrustedHosts).value -ne "<placeholder1>") {Set-Item WSMan:\localhost\Client\TrustedHosts "<placeholder1>"}
 
-Connect-MgGraph -TenantID "<placeholder>" -ClientID "<placeholder>" #use your auth method of choice
+Try {
+    Connect-MgGraph -TenantId "<placeholder2>" #authentication method of your choice
+} Catch {
+    Write-Host "Unable to connect to Microsoft Graph"
+    Exit 1
+}
 
-function Get-EntraDevice {
+
+function Get-EntraDevice{
 param($ComputerName)
 
     $URI = "https://graph.microsoft.com/beta/devices?filter=displayName eq '$ComputerName'"
 
     $result = Invoke-MgGraphRequest -Uri $URI -Method GET
-    if ($result.value){return $result.value}
+    if ($result.value){Return $result.value}
+    Remove-Variable result
+    Disconnect-MgGraph | Out-Null
     Write-Host "Unable to find Entra Device"
-    Exit 1
+    Exit 2
 }
  
-function Get-EntraDeviceLapsPassword {
+function Get-EntraDeviceLapsPassword{
 param($ComputerName)
 
     $device = Get-EntraDevice -ComputerName $ComputerName
 
     if ($null -ne $device){
-        $id = $device.deviceID
-        $URI = "https://graph.microsoft.com/beta/directory/deviceLocalCredentials/"+"$id"+"?`$select=credentials"
+        $URI = "https://graph.microsoft.com/beta/directory/deviceLocalCredentials/"+$device.deviceID+"?`$select=credentials"
         $result = Invoke-MgGraphRequest -Uri $URI -Method GET
         if ($result.credentials){
             $decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($result.credentials[0]['passwordBase64']))
-            return $decoded
+            Remove-Variable result
+            Return $decoded
         }
     }
+    Disconnect-MgGraph | Out-Null
     Write-Host "Unable to find Entra Device LAPS Credentials"
-    Exit 2
-}
-
-Function New-EntraDevicePSSession {
-param($ComputerName)
-
-    $username = $ComputerName + "\$Adminuser"
-    $password = Get-EntraDeviceLapsPassword -ComputerName $ComputerName | ConvertTo-SecureString -AsPlainText -Force -ErrorAction SilentlyContinue
-
-    if ($null -ne $password) {
-        [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $password)
-
-        return New-PSSession -ComputerName $ComputerName -Credential $credObject
-    }
-    Write-Host "Unable to create PS Session"
     Exit 3
 }
 
-$session = New-EntraDevicePSSession -ComputerName $ComputerName
+Function New-EntraDevicePSSession{
+param($ComputerName)
 
-if ($null -ne $session) {
-
-    Enter-PSSession $session
-
+    Try {
+        $username = $Computername + "\$Adminuser"
+        [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName,
+                                                                                              $(Get-EntraDeviceLapsPassword -ComputerName $ComputerName `
+                                                                                              | ConvertTo-SecureString -AsPlainText -Force -ErrorAction SilentlyContinue))
+    
+        $session =  new-pssession -ComputerName $ComputerName -Credential $credObject -SessionOption (New-PSSessionOption -IdleTimeout 60000)
+        Remove-Variable credObject
+        Return $session
+    } Catch {
+        Disconnect-MgGraph | Out-Null
+        Write-Host "Unable to create PS Session"
+        Exit 4
+    }
 }
 
-#don't forget to remove the pssession when you are done :)
+$session = New-EntraDevicePSSession -ComputerName $ComputerName
+Disconnect-MgGraph | Out-Null
+if ($null -ne $session) { 
+    Enter-PSSession $session
+}
+
+#don't forget to remove the pssession $session when you are done :)
